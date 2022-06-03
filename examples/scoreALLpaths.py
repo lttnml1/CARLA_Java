78750b8ff5f427835aaf9b3209930105c5dd6c6d
m.litton_local <m.litton_local@IT160248-csclpp.ern.nps.edu>
2022-06-03 14:19:20 -0700

    working

--- examples/scoreALLpaths.py
+++ examples/scoreALLpaths.py
@@ -38,23 +38,30 @@ def main():
     path = args.path
     
     try:
-        f = open(args.scores,"a")
+        
+
         for dirName, subdirList, fileList in os.walk(path):
             fileList.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
             for fileName in fileList:
-                print(fileName)
                 call_string2 = "C:/Users/m.litton_local/anaconda3/envs/carla_windows/python.exe c:/Users/m.litton_local/CARLA_Java/examples/graphPart_5_24_22.py --sync --loop --port " + str(args.port) + " --file " + path + fileName + " --no_render"
-                #call_string = "/home/littonml1/anaconda3/envs/carla/bin/python -W ignore /home/littonml1/CARLA_Java/examples/graphPart_5_24_22.py --sync --loop --port " + str(args.port) + " --file /home/littonml1/python_proj/Adversary1/" + fileName + " --no_render"
+                call_string = "/home/littonml1/anaconda3/envs/carla/bin/python -W ignore /home/littonml1/CARLA_Java/examples/graphPart_5_24_22.py --sync --loop --port " + str(args.port) + " --file /home/littonml1/python_proj/Adversary1/" + fileName + " --no_render"
                 #get return value of subprocess call
                 result=subprocess.Popen(call_string2, shell = True, stdout=subprocess.PIPE).communicate()[0].decode('ascii').strip()       
                 #append it to a file
-                if(not result): break
                 write_string = fileName.split("_")[1]+":"+result+"\n"
                 #f = open("/home/littonml1/python_proj/000_Adversary1_Scores", "a")
-                f.write(write_string)
+                #f = open("C:\\data\\000_Adversary1_Scores.txt", "a")
+                if(not result): 
+                    print("nothing")
+                    break
+                else: 
+                    f = open(args.scores,"a")
+                    f.write(write_string)
+                    f.close()
+        
+        
                 
         
-        f.close()
 
         #When done, put out file name where scores are
           

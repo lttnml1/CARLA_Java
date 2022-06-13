4beb2b4a5c2d4e256780472ffe8c2f37fe9f8102
lttnml1 <littonml1@gmail.com>
2022-06-13 06:32:04 -0700

    adv3

--- examples/Adversary3_Scenario.py
+++ examples/Adversary3_Scenario.py
@@ -142,13 +142,43 @@ class World(object):
         # Location(x=-70.731575, y=4.893159, z=-0.058300)       
         
     
-    def is_in_crosswalk(self, crosswalk, actor_location):
+    def is_in_crosswalk(self, actor_location):
         point = Point(actor_location.x,actor_location.y)
-        polygon_points = []
-        for pt in crosswalk:
-            polygon_points.append((pt.x,pt.y))
-        polygon = Polygon(polygon_points)
-        return polygon.contains(point)      
+
+        outer_polygon = []
+        outer_polygon.append(carla.Location(x=-97,y=26,z=1))
+        outer_polygon.append(carla.Location(x=-68.5,y=5.5,z=1))
+        outer_polygon.append(carla.Location(x=-68.5,y=-7.5,z=1))
+        outer_polygon.append(carla.Location(x=-96,y=-7.5,z=1))
+        outer_polygon.append(carla.Location(x=-97,y=26,z=1))
+
+        #for j in range (0,len(outer_polygon)-1):
+        #    self.world.debug.draw_line(outer_polygon[j]+carla.Location(z=1), outer_polygon[j+1]+carla.Location(z=1), thickness=0.2, color=carla.Color(255,0,0), life_time=20)
+        
+        inner_polygon = []
+        inner_polygon.append(carla.Location(x=-94,y=20,z=1))
+        inner_polygon.append(carla.Location(x=-71.6,y=3.6,z=1))
+        inner_polygon.append(carla.Location(x=-71.6,y=-4.5,z=1))
+        inner_polygon.append(carla.Location(x=-93.3,y=-4.5,z=1))
+        inner_polygon.append(carla.Location(x=-94,y=20,z=1))
+
+        #for j in range (0,len(inner_polygon)-1):
+        #    self.world.debug.draw_line(inner_polygon[j]+carla.Location(z=1), inner_polygon[j+1]+carla.Location(z=1), thickness=0.2, color=carla.Color(0,255,0), life_time=20)
+        inner_polygon_points = []
+        for pt in inner_polygon:
+            inner_polygon_points.append((pt.x,pt.y))
+        outer_polygon_points = []
+        for pt in outer_polygon:
+            outer_polygon_points.append((pt.x,pt.y))
+        polygon_outer = Polygon(outer_polygon_points)
+        polygon_inner = Polygon(inner_polygon_points)
+        if polygon_outer.contains(point):
+            if polygon_inner.contains(point):
+                return False
+            else:
+                return True
+        else:
+            return False
 
     def modify_vehicle_physics(self, actor):
         #If actor is not a vehicle, we cannot use the physics control
@@ -243,9 +273,9 @@ class Grid(object):
         if(draw_time > 0): Grid.draw_grid(self, draw_time) 
 
     def return_location_from_grid(self, i: int, j: int, draw_time: int = 0):
-        center_point_y = self.left + self.box_width*(j-1) + self.box_width/2
+        center_point_y = self.left + self.box_width*(j) + self.box_width/2
         #print(center_point_y)
-        center_point_x = self.top - self.box_height*(i-1) - self.box_height/2
+        center_point_x = self.top - self.box_height*(i) - self.box_height/2
         #print(center_point_x)
         location = carla.Location(x=center_point_x,y=center_point_y,z=1)
         #print(location)
@@ -279,7 +309,7 @@ class Grid(object):
 # ==============================================================================
 
 
-def game_loop(args):
+def categorical_game_loop(args):
     """
     Main loop of the simulation.
     """
@@ -296,51 +326,29 @@ def game_loop(args):
             if(args.no_render): settings.no_rendering_mode = True
             sim_world.apply_settings(settings)
 
-        grid = Grid(client.get_world(),-65,-100,-10,30)#, draw_time = 10)   
+        grid = Grid(client.get_world(),-65,-100,-10,30)#, draw_time = 60)   
         world = World(client.get_world(), grid, args)
 
         #set the view to the middle of the grid
         spectator = world.world.get_spectator()
         spectator.set_transform(carla.Transform(carla.Location(x=-100,y=14,z=50),carla.Rotation(roll=0, pitch=-70,yaw=0)))
 
-        outer_polygon = []
-        inner_polygon = []
-        outer_polygon.append(carla.Location(x=-97,y=26,z=1))
-        outer_polygon.append(carla.Location(x=-68.5,y=5.5,z=1))
-        outer_polygon.append(carla.Location(x=-68.5,y=-7.5,z=1))
-        outer_polygon.append(carla.Location(x=-96,y=-7.5,z=1))
-        outer_polygon.append(carla.Location(x=-97,y=26,z=1))
-
-        for j in range (0,len(outer_polygon)-1):
-            world.world.debug.draw_line(outer_polygon[j]+carla.Location(z=1), outer_polygon[j+1]+carla.Location(z=1), thickness=0.2, color=carla.Color(255,0,0), life_time=20)
         
-        inner_polygon.append(carla.Location(x=-94,y=20,z=1))
-        inner_polygon.append(carla.Location(x=-71.6,y=3.6,z=1))
-        inner_polygon.append(carla.Location(x=-71.6,y=-4.5,z=1))
-        inner_polygon.append(carla.Location(x=-93.3,y=-4.5,z=1))
-        inner_polygon.append(carla.Location(x=-94,y=20,z=1))
-
-        for j in range (0,len(inner_polygon)-1):
-            world.world.debug.draw_line(inner_polygon[j]+carla.Location(z=1), inner_polygon[j+1]+carla.Location(z=1), thickness=0.2, color=carla.Color(0,255,0), life_time=20)
-        
-        #for loc in outer_polygon:
-        #    world.world.debug.draw_point(loc,size=0.2,color=carla.Color(255,0,0),life_time=10)
-        
-        
+               
         #read the path the Adversary is supposed to take from the file
-        if "CategoricalDistrib" in args.file:
-            with open(args.file) as f:
-                lines = f.readlines()
-            point_array=[]
-            destination_array=[]
-            for line in lines:
-                sp = line.split(',')
-                point_array.append(sp[1])
-            for point in point_array:
-                i = math.floor(int(point)/20)
-                j = int(point) % 20
-                dest = grid.return_location_from_grid(i,j)
-                destination_array.append(dest)
+        with open(args.file) as f:
+            lines = f.readlines()
+        point_array=[]
+        destination_array=[]
+        for line in lines:
+            sp = line.split(',')
+            point_array.append(sp[1])
+        for point in point_array:
+            i = math.floor(int(point)/20)
+            j = int(point) % 20
+            dest = grid.return_location_from_grid(i,j)
+            destination_array.append(dest)
+        
         
         # Spawn the actors
         blueprints = world.world.get_blueprint_library()
@@ -353,7 +361,7 @@ def game_loop(args):
 
         Adversary3_spawn_point = carla.Transform(destination_array[0],carla.Rotation(roll=0,pitch=0,yaw=0))
         world.Adversary_3 = world.world.try_spawn_actor(Adversary3_blueprint,Adversary3_spawn_point)
-        grid.draw_location_on_grid(destination_array[0], draw_time = 5)
+        #grid.draw_location_on_grid(destination_array[0], draw_time = 5)
         world.modify_vehicle_physics(world.Adversary_3)
         
         #ego_spawn_point = carla.Transform(grid.return_location_from_grid(8,20),carla.Rotation(roll=0,pitch=0,yaw=-90))
@@ -369,7 +377,7 @@ def game_loop(args):
         #now intialize the agents
         dest_index = 1
         Adversary3_agent = SimpleAgent(world.Adversary_3, destination_array[dest_index], target_speed=8)
-        grid.draw_location_on_grid(destination_array[1], draw_time = 5)
+        #grid.draw_location_on_grid(destination_array[1], draw_time = 5)
                 
         #ego_agent = BasicAgent(world.ego, target_speed = 7)
         #ego_dest = carla.Location(x=-40,y=0,z=0)
@@ -389,37 +397,46 @@ def game_loop(args):
 
             #ego_loca = world.ego.get_location()
             Adversary3_loca = world.Adversary_3.get_location()
-            #if(world.is_in_crosswalk(cw,Adversary3_loca)):
-            #    print("**ADVERSARY3 IN CROSSWALK")
+            
             #distance = math.sqrt((ego_loca.x - Adversary1_loca.x)**2 + (ego_loca.y - Adversary1_loca.y)**2)
             #print(f"Distance between the two is {distance:8.4f}")
-            """
+               
             global MIN_DISTANCE
-            if(distance < MIN_DISTANCE):
-                MIN_DISTANCE = distance
+            global PATH_DISTANCE
+            #if(distance < MIN_DISTANCE):
+            #    MIN_DISTANCE = distance
             if(COLLISION):
                 #print("Ending simulation due to collision")
-                MIN_DISTANCE = -1
+                #MIN_DISTANCE = -1
                 #print(f"Minimum distance was: {MIN_DISTANCE}")
-                print(f"{MIN_DISTANCE:8.6f}")
+                PATH_DISTANCE += 100
+                print(f"{PATH_DISTANCE:8.6f}")
                 break
-            """
+            
             if Adversary3_agent.done():
                 if args.loop:
                     #print(world.world.get_snapshot().timestamp)
                     #print(f"Adversary 1 has reached destination {dest_index}:\tSpeed was supposed to be {speed_array[dest_index]}, it is actually {Adversary1_speed}")
                     #if(ego_loca.y > grid.left): 
                     #    i,j = grid.return_grid_from_location(world.ego.get_location())
-                    #    #print(f"Ego location: ({i},{j}), speed: {ego_speed}")                                
-                    if dest_index == (len(destination_array) - 2):
-                        print("Adversary 3's route is complete")
-                        #print(f"Minimum distance was: {MIN_DISTANCE:8.6f}")
+                    #    #print(f"Ego location: ({i},{j}), speed: {ego_speed}")
+                    
+                    if dest_index == (len(destination_array) - 1):
+                        #print("Adversary 3's route is complete")
+                        print(f"{PATH_DISTANCE:8.6f}")
                         #print(f"{MIN_DISTANCE:8.6f}")
                         break
                     dest_index = dest_index+1
                     new_dest = destination_array[dest_index]
-                    grid.draw_location_on_grid(new_dest, draw_time = 3)
+                    if(world.is_in_crosswalk(new_dest)):
+                        #world.world.debug.draw_point(new_dest,size=0.2,color=carla.Color(255,0,0),life_time=3)
+                        PATH_DISTANCE += 0#math.sqrt((new_dest.x - Adversary3_loca.x)**2 + (new_dest.y - Adversary3_loca.y)**2)-1
+                    else: 
+                        PATH_DISTANCE += math.sqrt((new_dest.x - Adversary3_loca.x)**2 + (new_dest.y - Adversary3_loca.y)**2)
+                        #grid.draw_location_on_grid(new_dest, draw_time = 3)
                     Adversary3_agent.set_destination(new_dest)
+                    
+                    
                     #new_speed =speed_array[dest_index] 
                     #Adversary1_agent.set_target_speed(new_speed)
                                         
@@ -434,7 +451,7 @@ def game_loop(args):
             #    world.ego.apply_control(ego_agent.run_step())   
             #else:
             #    world.ego.apply_control(ego_agent.add_emergency_stop(carla.VehicleControl()))        
-
+        
 
     
     finally:
@@ -448,6 +465,154 @@ def game_loop(args):
 
             world.destroy()
 
+def normal_game_loop(args):
+    """
+    Main loop of the simulation.
+    """
+    world = None
+    try:
+        client = carla.Client(args.host, args.port)
+        client.set_timeout(4.0)
+        sim_world = client.get_world()
+
+        if args.sync:
+            settings = sim_world.get_settings()
+            settings.synchronous_mode = True
+            settings.fixed_delta_seconds = 0.05
+            if(args.no_render): settings.no_rendering_mode = True
+            sim_world.apply_settings(settings)
+
+        grid = Grid(client.get_world(),-65,-100,-10,30)#, draw_time = 60)   
+        world = World(client.get_world(), grid, args)
+
+        #set the view to the middle of the grid
+        spectator = world.world.get_spectator()
+        spectator.set_transform(carla.Transform(carla.Location(x=-100,y=14,z=50),carla.Rotation(roll=0, pitch=-70,yaw=0)))
+
+        #read the path the Adversary is supposed to take from the file
+        with open(args.file) as f:
+            lines = f.readlines()
+        point_array=[]
+        speed_array=[]
+        destination_array=[]
+        for line in lines:
+            sp = line.split(',')
+            point_array.append(sp[1])
+            speed = float(sp[2]) * 3.6 * 10
+            if(speed < 1.0): speed_array.append(10.0)
+            else: speed_array.append(speed) 
+        for point in point_array:
+            i = math.floor(int(point)/20)
+            j = int(point) % 20
+            dest = grid.return_location_from_grid(i,j)
+            destination_array.append(dest)
+        
+        
+        # Spawn the actors
+        blueprints = world.world.get_blueprint_library()
+
+        Adversary3_blueprint=blueprints.filter("diamondback")[0]
+        Adversary3_blueprint.set_attribute('role_name', 'Adversary3')
+
+        ego_blueprint = blueprints.filter("vehicle.dodge.charger_police")[0]
+        ego_blueprint.set_attribute('role_name','ego')
+
+        Adversary3_spawn_point = carla.Transform(destination_array[0],carla.Rotation(roll=0,pitch=0,yaw=0))
+        world.Adversary_3 = world.world.try_spawn_actor(Adversary3_blueprint,Adversary3_spawn_point)
+        #grid.draw_location_on_grid(destination_array[0], draw_time = 5)
+        world.modify_vehicle_physics(world.Adversary_3)
+        
+        ego_spawn_point = carla.Transform(grid.return_location_from_grid(8,20),carla.Rotation(roll=0,pitch=0,yaw=-90))
+        world.ego = world.world.try_spawn_actor(ego_blueprint,ego_spawn_point)
+        world.modify_vehicle_physics(world.ego)   
+        world.collision_sensor = CollisionSensor(world.Adversary_3)    
+        
+        if world._args.sync:
+            world.world.tick()
+        else:
+            world.world.wait_for_tick()
+        
+        #now intialize the agents
+        dest_index = 1
+        Adversary3_agent = SimpleAgent(world.Adversary_3, destination_array[dest_index], target_speed=8)
+        #grid.draw_location_on_grid(destination_array[1], draw_time = 5)
+                
+        ego_agent = BasicAgent(world.ego, target_speed = 8,  opt_dict={'ignore_traffic_lights':'True'})
+        ego_dest = grid.return_location_from_grid(8,0)#carla.Location(x=-80,y=-20,z=0)
+        waypoint = world.world.get_map().get_waypoint(ego_dest,project_to_road=True, lane_type=(carla.LaneType.Driving))
+        ego_agent.set_destination(waypoint.transform.location)
+        
+        
+        while True:
+            if args.sync:
+                world.world.tick()
+            else:
+                world.world.wait_for_tick()
+            
+
+            Adversary3_speed = world.get_vehicle_speed(world.Adversary_3)
+            ego_speed = world.get_vehicle_speed(world.ego)    
+
+            ego_loca = world.ego.get_location()
+            Adversary3_loca = world.Adversary_3.get_location()
+            distance = math.sqrt((ego_loca.x - Adversary3_loca.x)**2 + (ego_loca.y - Adversary3_loca.y)**2)
+            #print(f"Distance between the two is {distance:8.4f}")
+               
+            global MIN_DISTANCE
+            if(distance < MIN_DISTANCE):
+                MIN_DISTANCE = distance
+            if(COLLISION):
+                #print("Ending simulation due to collision")
+                MIN_DISTANCE = -1
+                #print(f"Minimum distance was: {MIN_DISTANCE}")
+                print(f"{MIN_DISTANCE:8.6f}")
+                break
+            
+            if Adversary3_agent.done():
+                if args.loop:
+                    #print(world.world.get_snapshot().timestamp)
+                    #print(f"Adversary 1 has reached destination {dest_index}:\tSpeed was supposed to be {speed_array[dest_index]}, it is actually {Adversary1_speed}")
+                    #if(ego_loca.y > grid.left): 
+                    #    i,j = grid.return_grid_from_location(world.ego.get_location())
+                    #    #print(f"Ego location: ({i},{j}), speed: {ego_speed}")
+                    
+                    if dest_index == (len(destination_array) - 1):
+                        #print("Adversary 3's route is complete")
+                        #print(f"{PATH_DISTANCE:8.6f}")
+                        print(f"{MIN_DISTANCE:8.6f}")
+                        break
+                    dest_index = dest_index+1
+                    new_dest = destination_array[dest_index]
+                    #grid.draw_location_on_grid(new_dest, draw_time = 3)
+                    
+                    Adversary3_agent.set_destination(new_dest)
+                    new_speed =speed_array[dest_index] 
+                    Adversary3_agent.set_target_speed(new_speed)
+                                        
+                else:
+                    #print("The target has been reached, stopping the simulation")
+                    break
+            
+            control = Adversary3_agent.run_step()
+            control.manual_gear_shift = False
+            world.Adversary_3.apply_control(control)
+            if (ego_loca.y > grid.left-5):
+                world.ego.apply_control(ego_agent.run_step())   
+            else:
+                world.ego.apply_control(ego_agent.add_emergency_stop(carla.VehicleControl()))        
+        
+
+    
+    finally:
+        if world is not None:
+            settings = world.world.get_settings()
+            settings.synchronous_mode = False
+            settings.fixed_delta_seconds = None
+            #settings.no_rendering_mode = False
+            world.world.apply_settings(settings)
+            
+
+            world.destroy()
 # ==============================================================================
 # -- main() --------------------------------------------------------------
 # ==============================================================================
@@ -496,7 +661,6 @@ def main():
     logging.basicConfig(format='%(levelname)s: %(message)s', level=log_level)
 
     #logging.info('listening to server %s:%s', args.host, args.port)
-
     #print(__doc__)
 
     
@@ -505,7 +669,12 @@ def main():
         COLLISION = False
         global MIN_DISTANCE
         MIN_DISTANCE = 999.99999
-        game_loop(args)
+        global PATH_DISTANCE
+        PATH_DISTANCE = 0.0
+        if "Categorical" in args.file:
+            categorical_game_loop(args)
+        else:
+            normal_game_loop(args)
 
     except KeyboardInterrupt:
         print('\nCancelled by user. Bye!')
--- examples/scoreALLpaths.py
+++ examples/scoreALLpaths.py
@@ -43,12 +43,12 @@ def main():
         for dirName, subdirList, fileList in os.walk(path):
             fileList.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
             for fileName in fileList:
-                call_string2 = "C:/Users/m.litton_local/anaconda3/envs/carla_windows/python.exe c:/Users/m.litton_local/CARLA_Java/examples/graphPart_5_24_22.py --sync --loop --port " + str(args.port) + " --file " + path + fileName + " --no_render"
+                call_string2 = "C:/Users/m.litton_local/anaconda3/envs/carla_windows/python.exe c:/Users/m.litton_local/CARLA_Java/examples/Adversary3_Scenario.py --sync --loop --port " + str(args.port) + " --file " + path + fileName + " --no_render"
                 call_string = "/home/littonml1/anaconda3/envs/carla/bin/python -W ignore /home/littonml1/CARLA_Java/examples/graphPart_5_24_22.py --sync --loop --port " + str(args.port) + " --file /home/littonml1/python_proj/Adversary1/" + fileName + " --no_render"
                 #get return value of subprocess call
                 result=subprocess.Popen(call_string2, shell = True, stdout=subprocess.PIPE).communicate()[0].decode('ascii').strip()       
                 #append it to a file
-                write_string = fileName.split("_")[1]+":"+result+"\n"
+                write_string = fileName.split("_")[2]+":"+result+"\n"
                 #f = open("/home/littonml1/python_proj/000_Adversary1_Scores", "a")
                 #f = open("C:\\data\\000_Adversary1_Scores.txt", "a")
                 if(not result): 

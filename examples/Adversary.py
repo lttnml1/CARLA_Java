cfec58f138b4a638955b14fbba47165255fe567d
lttnml1 <littonml1@gmail.com>
2022-07-21 10:13:54 -0700

    crash

--- examples/Adversary.py
+++ examples/Adversary.py
@@ -275,7 +275,7 @@ class CollisionSensor(object):
         if len(self.history) > 4000:
             self.history.pop(0)
         #if "vehicle" in str(event.other_actor.type_id):
-        #    print(f"Collision with {event.other_actor.type_id}")
+        #print(f"Collision with {event.other_actor.type_id}")
         #actor_type = ' '.join(event.other_actor.type_id.replace('_', '.').title().split('.')[1:])
         #print("Collision with %r" % actor_type)
 
